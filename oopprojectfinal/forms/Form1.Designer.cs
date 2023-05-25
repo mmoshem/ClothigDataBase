f8d8b2f3344c5e954effc02284aaf3eaee937ce1
Daniel Dolberg <dolbergdani@gmail.com>
2023-05-25 17:21:45 +0300

    fixed image problem

--- oopprojectfinal/forms/Form1.Designer.cs
+++ oopprojectfinal/forms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label_select_Item = new System.Windows.Forms.Label();
             this.comboBoxItem = new System.Windows.Forms.ComboBox();
             this.panelForm = new System.Windows.Forms.Panel();
@@ -36,16 +37,19 @@
             this.dataGrid = new System.Windows.Forms.DataGridView();
             this.buttonClear = new System.Windows.Forms.Button();
             this.showPic = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label_select_Item
             // 
             this.label_select_Item.AutoSize = true;
             this.label_select_Item.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_select_Item.Location = new System.Drawing.Point(24, 43);
+            this.label_select_Item.Location = new System.Drawing.Point(32, 53);
+            this.label_select_Item.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_select_Item.Name = "label_select_Item";
-            this.label_select_Item.Size = new System.Drawing.Size(88, 20);
+            this.label_select_Item.Size = new System.Drawing.Size(108, 25);
             this.label_select_Item.TabIndex = 0;
             this.label_select_Item.Text = "Select item";
             // 
@@ -56,24 +60,27 @@
             "Shoe",
             "Shirt",
             "Pants"});
-            this.comboBoxItem.Location = new System.Drawing.Point(118, 42);
+            this.comboBoxItem.Location = new System.Drawing.Point(157, 52);
+            this.comboBoxItem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxItem.Name = "comboBoxItem";
-            this.comboBoxItem.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxItem.Size = new System.Drawing.Size(160, 24);
             this.comboBoxItem.TabIndex = 2;
             this.comboBoxItem.SelectedIndexChanged += new System.EventHandler(this.comboBoxItem_SelectedIndexChanged);
             // 
             // panelForm
             // 
-            this.panelForm.Location = new System.Drawing.Point(28, 100);
+            this.panelForm.Location = new System.Drawing.Point(37, 123);
+            this.panelForm.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelForm.Name = "panelForm";
-            this.panelForm.Size = new System.Drawing.Size(274, 378);
+            this.panelForm.Size = new System.Drawing.Size(365, 465);
             this.panelForm.TabIndex = 12;
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(258, 29);
+            this.buttonAdd.Location = new System.Drawing.Point(344, 36);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(75, 44);
+            this.buttonAdd.Size = new System.Drawing.Size(100, 54);
             this.buttonAdd.TabIndex = 14;
             this.buttonAdd.Text = "Add";
             this.buttonAdd.UseVisualStyleBackColor = true;
@@ -81,9 +88,10 @@
             // 
             // update_button
             // 
-            this.update_button.Location = new System.Drawing.Point(371, 29);
+            this.update_button.Location = new System.Drawing.Point(495, 36);
+            this.update_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.update_button.Name = "update_button";
-            this.update_button.Size = new System.Drawing.Size(129, 44);
+            this.update_button.Size = new System.Drawing.Size(172, 54);
             this.update_button.TabIndex = 16;
             this.update_button.Text = "update";
             this.update_button.UseVisualStyleBackColor = true;
@@ -92,18 +100,20 @@
             // dataGrid
             // 
             this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(347, 103);
+            this.dataGrid.Location = new System.Drawing.Point(463, 127);
+            this.dataGrid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowHeadersWidth = 82;
-            this.dataGrid.Size = new System.Drawing.Size(815, 375);
+            this.dataGrid.Size = new System.Drawing.Size(1087, 462);
             this.dataGrid.TabIndex = 0;
             this.dataGrid.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGrid_RowHeaderMouseClick);
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(528, 29);
+            this.buttonClear.Location = new System.Drawing.Point(704, 36);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(115, 44);
+            this.buttonClear.Size = new System.Drawing.Size(153, 54);
             this.buttonClear.TabIndex = 17;
             this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
@@ -111,19 +121,31 @@
             // 
             // showPic
             // 
-            this.showPic.Location = new System.Drawing.Point(676, 29);
+            this.showPic.Location = new System.Drawing.Point(901, 36);
+            this.showPic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.showPic.Name = "showPic";
-            this.showPic.Size = new System.Drawing.Size(75, 44);
+            this.showPic.Size = new System.Drawing.Size(100, 54);
             this.showPic.TabIndex = 18;
             this.showPic.Text = "Show Pic";
             this.showPic.UseVisualStyleBackColor = true;
             this.showPic.Click += new System.EventHandler(this.showPic_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 611);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1513, 428);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(642, 406);
+            this.ClientSize = new System.Drawing.Size(1583, 1025);
             this.Controls.Add(this.showPic);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.update_button);
@@ -132,10 +154,13 @@
             this.Controls.Add(this.label_select_Item);
             this.Controls.Add(this.panelForm);
             this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +176,7 @@
         private System.Windows.Forms.DataGridView dataGrid;
         private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.Button showPic;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- oopprojectfinal/models/pants.cs
+++ oopprojectfinal/models/pants.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,16 +15,17 @@ namespace oopprojectfinal
 
         public override void showPic()
         {
+            /*
             using (Form form = new Form())
             {
-                //string relativePath = @"Images\Pants\sp-pgm-dc-11.jpg"; // Provide the correct relative path to the image file
+                string relativePath = @"images/pants/first"; // Provide the correct relative path to the image file
 
-                string imagePath = System.IO.Path.Combine(Application.StartupPath/*, relativePath*/);
+                string imagePath = System.IO.Path.Combine(Application.StartupPath, relativePath);
                 //@"C:\Users\mmosh\Desktop\oopprojectfinal\oopprojectfinal\imeges\pants\365132_308_XL.jpg"
 
 
                 PictureBox pb = new PictureBox();
-                pb.Image = Image.FromFile(@"C:\Users\mmosh\Desktop\oopprojectfinal\oopprojectfinal\imeges\pants\first.jpg");
+                pb.Image = Image.FromFile(imagePath);
                 pb.Width = 1000;
                 pb.Height = 1000;
                 form.StartPosition = FormStartPosition.WindowsDefaultBounds;
@@ -33,12 +35,19 @@ namespace oopprojectfinal
                 form.ShowDialog();
             }
             //throw new NotImplementedException();
+            */
         }
         public Pants(string item, string gender,  string color, string brand, double price, string fubricType,string clothesSize , string pantsType) : base( item, gender,color, brand, price, fubricType, clothesSize)
         {
             this.pantsType = pantsType;
-            
-            pb.Image = Image.FromFile(@"C:\Users\mmosh\Desktop\oopprojectfinal\oopprojectfinal\imeges\pants\first.jpg");
+
+            string imagePath = Application.StartupPath;
+            imagePath = Directory.GetParent(imagePath).FullName;
+            imagePath = Directory.GetParent(imagePath).FullName;
+            imagePath += @"\images\pants\first.jpg";
+
+
+            pb.Image = Image.FromFile(imagePath);
             pb.SizeMode = PictureBoxSizeMode.StretchImage;//makes the pic in the size of the image
             pb.Size = new Size(100, 100);//size of the frame 
             pb.Location = new Point(0, 0);
