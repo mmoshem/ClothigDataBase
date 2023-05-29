7dfe8770f366540f5ec104ca1595abe00ae53ce1
Daniel Dolberg <66477511+DanielDolberg@users.noreply.github.com>
2023-05-29 17:13:45 +0300

    form1

--- oopprojectfinal/forms/Form1.Designer.cs
+++ oopprojectfinal/forms/Form1.Designer.cs
@@ -48,10 +48,10 @@
             // 
             this.label_select_Item.AutoSize = true;
             this.label_select_Item.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_select_Item.Location = new System.Drawing.Point(48, 83);
-            this.label_select_Item.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_select_Item.Location = new System.Drawing.Point(36, 66);
+            this.label_select_Item.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_select_Item.Name = "label_select_Item";
-            this.label_select_Item.Size = new System.Drawing.Size(173, 37);
+            this.label_select_Item.Size = new System.Drawing.Size(133, 29);
             this.label_select_Item.TabIndex = 0;
             this.label_select_Item.Text = "Select item";
             // 
@@ -62,41 +62,42 @@
             "Shoe",
             "Shirt",
             "Pants"});
-            this.comboBoxItem.Location = new System.Drawing.Point(236, 81);
-            this.comboBoxItem.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBoxItem.Location = new System.Drawing.Point(177, 65);
+            this.comboBoxItem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBoxItem.Name = "comboBoxItem";
-            this.comboBoxItem.Size = new System.Drawing.Size(238, 33);
+            this.comboBoxItem.Size = new System.Drawing.Size(180, 28);
             this.comboBoxItem.TabIndex = 2;
             this.comboBoxItem.SelectedIndexChanged += new System.EventHandler(this.comboBoxItem_SelectedIndexChanged);
             // 
             // panelForm
             // 
-            this.panelForm.Location = new System.Drawing.Point(56, 192);
-            this.panelForm.Margin = new System.Windows.Forms.Padding(6);
+            this.panelForm.Location = new System.Drawing.Point(42, 154);
+            this.panelForm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelForm.Name = "panelForm";
-            this.panelForm.Size = new System.Drawing.Size(548, 727);
+            this.panelForm.Size = new System.Drawing.Size(411, 582);
             this.panelForm.TabIndex = 12;
             // 
             // buttonAdd
             // 
+            this.buttonAdd.BackColor = System.Drawing.SystemColors.Control;
             this.buttonAdd.ForeColor = System.Drawing.Color.Coral;
-            this.buttonAdd.Location = new System.Drawing.Point(717, 63);
-            this.buttonAdd.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonAdd.Location = new System.Drawing.Point(538, 50);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(173, 67);
+            this.buttonAdd.Size = new System.Drawing.Size(130, 54);
             this.buttonAdd.TabIndex = 14;
             this.buttonAdd.Text = "Add";
-            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.UseVisualStyleBackColor = false;
             this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // update_button
             // 
-            this.update_button.BackColor = System.Drawing.Color.White;
+            this.update_button.BackColor = System.Drawing.SystemColors.Control;
             this.update_button.ForeColor = System.Drawing.Color.YellowGreen;
-            this.update_button.Location = new System.Drawing.Point(1010, 63);
-            this.update_button.Margin = new System.Windows.Forms.Padding(6);
+            this.update_button.Location = new System.Drawing.Point(758, 50);
+            this.update_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.update_button.Name = "update_button";
-            this.update_button.Size = new System.Drawing.Size(173, 67);
+            this.update_button.Size = new System.Drawing.Size(130, 54);
             this.update_button.TabIndex = 16;
             this.update_button.Text = "update";
             this.update_button.UseVisualStyleBackColor = false;
@@ -105,45 +106,47 @@
             // dataGrid
             // 
             this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(694, 198);
-            this.dataGrid.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGrid.Location = new System.Drawing.Point(520, 158);
+            this.dataGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowHeadersWidth = 82;
-            this.dataGrid.Size = new System.Drawing.Size(1766, 722);
+            this.dataGrid.Size = new System.Drawing.Size(1324, 578);
             this.dataGrid.TabIndex = 0;
             this.dataGrid.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGrid_RowHeaderMouseClick);
             // 
             // buttonClear
             // 
+            this.buttonClear.BackColor = System.Drawing.SystemColors.Control;
             this.buttonClear.ForeColor = System.Drawing.Color.DarkRed;
-            this.buttonClear.Location = new System.Drawing.Point(1594, 63);
-            this.buttonClear.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClear.Location = new System.Drawing.Point(1196, 50);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(173, 67);
+            this.buttonClear.Size = new System.Drawing.Size(130, 54);
             this.buttonClear.TabIndex = 17;
             this.buttonClear.Text = "Clear";
-            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.UseVisualStyleBackColor = false;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // showPic
             // 
+            this.showPic.BackColor = System.Drawing.SystemColors.Control;
             this.showPic.ForeColor = System.Drawing.Color.BlueViolet;
-            this.showPic.Location = new System.Drawing.Point(1309, 63);
-            this.showPic.Margin = new System.Windows.Forms.Padding(6);
+            this.showPic.Location = new System.Drawing.Point(982, 50);
+            this.showPic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.showPic.Name = "showPic";
-            this.showPic.Size = new System.Drawing.Size(173, 67);
+            this.showPic.Size = new System.Drawing.Size(130, 54);
             this.showPic.TabIndex = 18;
             this.showPic.Text = "Show Pic";
-            this.showPic.UseVisualStyleBackColor = true;
+            this.showPic.UseVisualStyleBackColor = false;
             this.showPic.Click += new System.EventHandler(this.showPic_Click);
             // 
             // pictureHolder
             // 
             this.pictureHolder.Image = ((System.Drawing.Image)(resources.GetObject("pictureHolder.Image")));
-            this.pictureHolder.Location = new System.Drawing.Point(56, 1011);
-            this.pictureHolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureHolder.Location = new System.Drawing.Point(42, 809);
+            this.pictureHolder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureHolder.Name = "pictureHolder";
-            this.pictureHolder.Size = new System.Drawing.Size(2404, 572);
+            this.pictureHolder.Size = new System.Drawing.Size(1803, 458);
             this.pictureHolder.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureHolder.TabIndex = 19;
             this.pictureHolder.TabStop = false;
@@ -152,10 +155,10 @@
             // 
             this.saveButton.BackColor = System.Drawing.Color.SteelBlue;
             this.saveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.saveButton.Location = new System.Drawing.Point(1822, 34);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(6);
+            this.saveButton.Location = new System.Drawing.Point(1366, 27);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(173, 67);
+            this.saveButton.Size = new System.Drawing.Size(130, 54);
             this.saveButton.TabIndex = 20;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = false;
@@ -165,10 +168,10 @@
             // 
             this.loadButton.BackColor = System.Drawing.Color.LimeGreen;
             this.loadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.loadButton.Location = new System.Drawing.Point(1822, 113);
-            this.loadButton.Margin = new System.Windows.Forms.Padding(6);
+            this.loadButton.Location = new System.Drawing.Point(1366, 90);
+            this.loadButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.loadButton.Name = "loadButton";
-            this.loadButton.Size = new System.Drawing.Size(173, 67);
+            this.loadButton.Size = new System.Drawing.Size(130, 54);
             this.loadButton.TabIndex = 21;
             this.loadButton.Text = "Load";
             this.loadButton.UseVisualStyleBackColor = false;
@@ -176,10 +179,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(2052, 1493);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1443, 840);
             this.Controls.Add(this.loadButton);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.pictureHolder);
@@ -192,7 +195,7 @@
             this.Controls.Add(this.panelForm);
             this.Controls.Add(this.dataGrid);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Clohing Data Base";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
